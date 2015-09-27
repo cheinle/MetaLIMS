1c85f63af5e9f2c6c4482cea46013425696a4980
cheinle <cheinle@ntu.edu.sg>
2015-09-27 03:57:27 -0700

    copied the same photo for 2-4 and changed db connection for home

--- database_connection.php
+++ database_connection.php
@@ -1,4 +1,10 @@
 <?php 
-	include('/xampp_2/config/connect_am_prod.php');
+	$work = 'false';
+	if($work == 'true'){
+		include('/xampp_2/config/connect_am_prod.php');
+	}
+	else{
+		include('/xampp/config/connection.txt');
+	}
 	$admin_user = 'cheinle@ntu.edu.sg';
 ?>
