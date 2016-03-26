a301867ac2bd5d9fce4064c507f4391f5365a317
cheinle <cheinle@ntu.edu.sg>
2016-03-26 06:25:49 -0700

    updated logout to use non-session variable for logout path

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+include('index.php');
 # Start Session
 session_start();
 //This would delete all the session keys
 session_destroy(); 
-$url = $_SERVER["HTTP_HOST"].$_SESSION['link_root']."login.php"; 
+$url = $_SERVER["HTTP_HOST"].$logout_path."login.php"; 
 header("Location: http://".$url);
 exit();
 ?>
