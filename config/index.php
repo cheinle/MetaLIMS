7824e90f3adf896c74e729830514d273c52b7c12
cheinle <cheinle@ntu.edu.sg>
2016-03-26 08:15:36 -0700

    added if(!isset($_SESSION)) { session_start(); } indexes

--- config/index.php
+++ config/index.php
@@ -1,6 +1,6 @@
 <?php
 include(INCLUDE_ROOT.'/config/path.php');
-session_start();
+if(!isset($_SESSION)) { session_start(); }
 if(!isset($_SESSION['username'])){
 	$url = $_SERVER["HTTP_HOST"].LINK_ROOT."login.php"; 
 	header("Location: http://".$url);
--- sequencing/query_past_seq_submissions/index.php
+++ sequencing/query_past_seq_submissions/index.php
@@ -1,6 +1,6 @@
 <?php
 include('../../index.php');
-session_start();
+if(!isset($_SESSION)) { session_start(); }
 if(!isset($_SESSION['username'])){
 	$url = $_SERVER["HTTP_HOST"].$logout_path."login.php"; 
 	header("Location: http://".$url);
