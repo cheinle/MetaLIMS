7388c50ff901a1c8c075b854da3c20d70e0f922c
Cassie <cheinle@ntu.edu.sg>
2016-03-30 18:44:10 +0800

    updated database connection file so can just remove my stuff when ready

--- database_connection.php
+++ database_connection.php
@@ -11,5 +11,5 @@
 	
 	//please change path to point to where you have stored your database connection 
 		//Ensure that database connection is outside of document root directory
-	//include('/mypath/config/connection.txt');
+	//include('/my/connection/path/example_connection.txt');
 ?>
