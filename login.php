e9e8408b3be4e4e2ac4e6ed31b38612f57c6a473
Cassie <cheinle@ntu.edu.sg>
2016-05-23 09:40:52 +0800

    login.php for users

--- login.php
+++ login.php
@@ -93,13 +93,11 @@ try{
 					 * Change this to point to where these files are stored in your document root directory. Leave as '/'
 					 * if files are in document root
 					 * *************************************************************************************************/
-					//$logout_path = '/series/dynamic/am_production/'; /*change here*/
+					
 					//************CREATE PATH VARIABLES*******//
-					//define('INCLUDE_PATH', $_SERVER['DOCUMENT_ROOT'].'/series/dynamic/am_production/');
-					//$_SESSION['include_path'] = $_SERVER['DOCUMENT_ROOT'].'/series/dynamic/am_production/';
-	
-					//define('LINK_ROOT', '/series/dynamic/am_production/');
-					//$_SESSION['link_root'] = '/series/dynamic/am_production/';
+					$logout_path = '/my/web/subdirectory/'; /*change here*/
+					$_SESSION['include_path'] = $_SERVER['DOCUMENT_ROOT'].'/my/web/subdirectory/';
+					$_SESSION['link_root'] = '/my/web/subdirectory/';
 					/////////////////////////////////////////////
 
 						
