23b45c7f5d9522a20cce96a914373aa0e4013737
Cassie <cheinle@ntu.edu.sg>
2016-05-24 10:46:56 +0800

    depricating page

--- path.php
+++ path.php
@@ -1,11 +1,13 @@
 <?php
-	if(!isset($_SESSION)) { session_start(); }
+	//Page No Longer In Use
+
+	//if(!isset($_SESSION)) { session_start(); }
     /***************************************************************************************************
 	 * Change this to point to where these files are stored in your document root directory. Leave as '/'
 	 * if files are in document root
 	 * *************************************************************************************************/
-	$logout_path = '/series/dynamic/am_production/'; /*change here*/
-	$_SESSION['include_path'] = $_SERVER['DOCUMENT_ROOT'].'/series/dynamic/am_production/';/*change here*/
-	$_SESSION['link_root'] = '/series/dynamic/am_production/';/*change here*/
+	//$logout_path = '/series/dynamic/am_production/'; /*change here*/
+	//$_SESSION['include_path'] = $_SERVER['DOCUMENT_ROOT'].'/series/dynamic/am_production/';/*change here*/
+	//$_SESSION['link_root'] = '/series/dynamic/am_production/';/*change here*/
 
 ?>
