5805140874a67c6420581e7ad6004e2afb726638
Cassie <cheinle@ntu.edu.sg>
2016-05-24 16:00:12 +0800

    testing gitignore

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,16 @@
 <?php 
+
+	/**my connection. dont forget to remove**/
+	$work = 'true';
+	if($work == 'true'){
+		include('/xampp_2/config/connect_am_prod.php');
+	}
+	else{
+		include('/xampp/config/connection.txt');
+	}
+	
 	//please change path to point to where you have stored your database connection 
-	//Ensure that database connection is outside of document root directory
-	
-	include('/my/connection/path/example_connection.txt');
-	
+		//Ensure that database connection is outside of document root directory
+	//include('/my/connection/path/example_connection.txt');
 ?>
+
