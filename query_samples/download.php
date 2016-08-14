1f55bb07481b88c013256503e66c744cf27cd4fb
cheinle <cheinle@users.noreply.github.com>
2016-08-14 08:06:46 -0700

    Update download.php
    explicit char encoding for download

--- query_samples/download.php
+++ query_samples/download.php
@@ -12,17 +12,9 @@ if ($fd = fopen ($fullPath, "r")) {
     $fsize = filesize($fullPath);
     $path_parts = pathinfo($fullPath);
     $ext = strtolower($path_parts["extension"]);
-    switch ($ext) {
-        case "pdf":
-        header("Content-type: application/pdf");
-        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
-        break;
-        // add more headers for other content types here
-        default;
-        header("Content-type: application/octet-stream");
-        header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
-        break;
-    }
+    #header("Content-Type: application/vnd.ms-excel");
+	header("Content-type: application/vnd.ms-excel; charset=UTF-8" );
+    header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
     header("Content-length: $fsize");
     header("Cache-control: private"); //use this to open files directly
     while(!feof($fd)) {
