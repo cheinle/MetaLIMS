fc951b53309d046d23d48227544f1710ba23a6f5
unknown <cheinle@SCELSE-2014-211.staff.main.ntu.edu.sg>
2017-03-17 19:55:02 +0800

    "change volumes to type d. update bulk dna checks to fix javascript error for radio and checkbox being checked in lower table

--- sample_insert/form_insert_submit.php
+++ sample_insert/form_insert_submit.php
@@ -210,7 +210,8 @@
 						}
 						
 						else{
-							 $stmt2 -> bind_param('sssssssssssdsiisdsiississsssss', $p_sample_name, $p_loc,$p_rloc, $p_collName, $p_dExtKit, $p_rExtKit, $p_seqInfo, $p_anPipe, $p_barcode, $p_sType, $p_projName, $p_dConc,$p_dInstru,$p_dVol,$p_dVol_quant,$p_d_extr_date,$p_rConc,$p_rInstru,$p_rVol,$p_rVol_quant,$p_r_extr_date,$p_notes,$p_sample_number,$p_entered_by,$sample_sort,$p_orig_time_stamp,$p_media,$p_dExtrName,$p_rExtrName,$seq_id);
+							 //$stmt2 -> bind_param('sssssssssssdsiisdsiississsssss', $p_sample_name, $p_loc,$p_rloc, $p_collName, $p_dExtKit, $p_rExtKit, $p_seqInfo, $p_anPipe, $p_barcode, $p_sType, $p_projName, $p_dConc,$p_dInstru,$p_dVol,$p_dVol_quant,$p_d_extr_date,$p_rConc,$p_rInstru,$p_rVol,$p_rVol_quant,$p_r_extr_date,$p_notes,$p_sample_number,$p_entered_by,$sample_sort,$p_orig_time_stamp,$p_media,$p_dExtrName,$p_rExtrName,$seq_id);
+							   $stmt2 -> bind_param('sssssssssssdsddsdsddssisssssss', $p_sample_name, $p_loc,$p_rloc, $p_collName, $p_dExtKit, $p_rExtKit, $p_seqInfo, $p_anPipe, $p_barcode, $p_sType, $p_projName, $p_dConc,$p_dInstru,$p_dVol,$p_dVol_quant,$p_d_extr_date,$p_rConc,$p_rInstru,$p_rVol,$p_rVol_quant,$p_r_extr_date,$p_notes,$p_sample_number,$p_entered_by,$sample_sort,$p_orig_time_stamp,$p_media,$p_dExtrName,$p_rExtrName,$seq_id);
 							 if(!$stmt2 -> execute()){
 							 	$insert_check = 'false';
 							 	throw new Exception("Execution Failure: Unable To Insert Into Main Sample Table");
